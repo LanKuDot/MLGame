9b6eba725dee99c4b0a9cf4e82fd597d3659046a
LanKuDot <airlanser@gmail.com>
2019-08-23 15:06:17 +0800

    Start the mode function of the game directly

--- MLGame.py
+++ MLGame.py
@@ -5,7 +5,13 @@ if __name__ == "__main__":
 	try:
 		config = gameconfig.get_game_config()
 		game = importlib.import_module("{}.main".format(config.game_name))
+
+		if config.game_mode == gameconfig.GameMode.MANUAL:
+			game.manual_mode(config)
+		elif config.game_mode == gameconfig.GameMode.ML:
+			game.ml_mode(config)
 	except ModuleNotFoundError:
 		print("Error: Game \"{}\" is not found.".format(config.game_name))
-	else:
-		game.execute(config)
+	except AttributeError:
+		print("Error: Game \"{}\" does not provide {} mode." \
+			.format(config.game_name, config.game_mode.name.lower()))
--- arkanoid/main.py
+++ arkanoid/main.py
@@ -1,61 +1,24 @@
-from essential.gameconfig import GameMode
+from essential.gameconfig import GameConfig
 
-def execute(config):
-	"""Start the game in the selected mode
-
-	An additional game parameter (stored in options.game_params)
-	for the game arkanoid is the level.
-
-	@param config The game configuration specified in the command line
-	"""
-	try:
-		level = int(config.game_params[0])
-		if level < 1:
-			raise ValueError
-	except IndexError:
-		print("Level is not specified. Set to 1.")
-		level = 1
-	except ValueError:
-		print("Invalid level value. Set to 1.")
-		level = 1
-
-	if config.game_mode == GameMode.MANUAL:
-		_manual_mode(config.fps, level, config.record_progress)
-	else:
-		_ml_mode(config.fps, level, config.input_scripts[0], \
-			config.record_progress, config.one_shot_mode, \
-			config.transition_channel)
-
-def _ml_mode(fps, level, input_script = "ml_play_template.py", \
-	record_progress = False, one_shot_mode = False, transition_channel = None):
+def ml_mode(config: GameConfig):
 	"""Start the game in the machine learning mode
 
 	Create a game and a machine learning processes.
-	The main process will run the game process.
-	If the `server_info` is not None, the main process will be the transition process,
-	which will pass the game progress to the remote server.
-
-	After quit from the drawing loop,
-	the created processes will be terminated.
-
-	@param fps Specify the updating rate of the game
-	@param level Specify the level of the game
-	@param input_script Specify the script for the ml process
-	@param record_progress Specify whether to record the game progress or not
-	@param one_shot_mode Specify whether to run the game only once or not
-	@param transition_channel Specify a list [server_ip, server_port, channel_name]
 	"""
-	to_transition = True if transition_channel else False
+	level = _get_level(config.game_params)
+	to_transition = True if config.transition_channel else False
 
 	from essential.process import ProcessManager
 
 	process_manager = ProcessManager()
 	process_manager.set_game_process(_start_game_process, \
-		args = (fps, level, record_progress, one_shot_mode, to_transition))
-	process_manager.add_ml_process("arkanoid.ml.{}".format(input_script.split('.')[0]), "ml")
+		args = (config.fps, level, \
+		config.record_progress, config.one_shot_mode, to_transition))
+	process_manager.add_ml_process("arkanoid.ml.{}" \
+		.format(config.input_scripts[0].split('.')[0]), "ml")
 
-	if transition_channel:
-		process_manager.set_transition_process(*transition_channel)
+	if to_transition:
+		process_manager.set_transition_process(*config.transition_channel)
 
 	process_manager.start()
 
@@ -74,18 +37,31 @@ def _start_game_process(fps, level, record_progress, one_shot_mode, to_transitio
 	game = Arkanoid(fps, level, record_handler, one_shot_mode, to_transition)
 	game.game_loop()
 
-def _manual_mode(fps, level, record_progress = False):
+def manual_mode(config: GameConfig):
 	"""Play the game as a normal game
-
-	@param fps Specify the updating rate of the game
-	@param level Specify the level of the game
-	@param record_progress Specify whether to record the game progress or not
 	"""
 	from .game.arkanoid import Arkanoid
 
-	record_handler = _get_record_handler(record_progress)
-	game = Arkanoid(fps, level, record_handler)
+	level = _get_level(config.game_params)
+	record_handler = _get_record_handler(config.record_progress)
+	game = Arkanoid(config.fps, level, record_handler)
 	game.game_loop()
+
+def _get_level(game_params):
+	"""Get the level from the parameter
+	"""
+	try:
+		level = int(game_params[0])
+		if level < 1:
+			raise ValueError
+	except IndexError:
+		print("Level is not specified. Set to 1.")
+		level = 1
+	except ValueError:
+		print("Invalid level value. Set to 1.")
+		level = 1
+
+	return level
 
 def _get_record_handler(record_progress: bool):
 	"""Return the handler for record the game progress
