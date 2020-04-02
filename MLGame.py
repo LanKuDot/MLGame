d5a122cb36dfa44d0d3b52e30d9a65f8b74280cd
LanKuDot <airlanser@gmail.com>
2020-04-02 15:46:21 +0800

    Execute the game from the execution.py

--- MLGame.py
+++ MLGame.py
@@ -1,22 +1,4 @@
-from mlgame import gameconfig
-from mlgame.exception import GameConfigError
-import importlib
+from mlgame.execution import execute
 
 if __name__ == "__main__":
-    try:
-        config = gameconfig.get_game_config()
-        game = importlib.import_module("games.{}.main".format(config.game_name))
-
-        if config.game_mode == gameconfig.GameMode.MANUAL:
-            game_execution = game.manual_mode
-        elif config.game_mode == gameconfig.GameMode.ML:
-            game_execution = game.ml_mode
-    except GameConfigError as e:
-        print("Error: " + str(e))
-    except ModuleNotFoundError:
-        print("Error: Game \"{}\" is not found.".format(config.game_name))
-    except AttributeError as e:
-        print("Error: Game \"{}\" does not provide {} mode." \
-            .format(config.game_name, config.game_mode.name.lower()))
-    else:
-        game_execution(config)
+    execute()
---
+++ mlgame/execution.py
@@ -0,0 +1,157 @@
+"""
+"""
+import importlib
+import sys
+
+from .gameconfig import get_game_config, GameMode, GameConfig
+from .exception import GameConfigError
+from .utils.argparser_generator import get_parser_from_dict
+
+def execute():
+    """
+    Parse the execution command and execute the game
+    """
+    try:
+        game_config = _parse_game_config()
+    except GameConfigError as e:
+        print("Error:", e)
+        sys.exit(1)
+
+    try:
+        _game_execution(game_config)
+    except GameConfigError as e:
+        print("Error:", e)
+        sys.exit(1)
+
+def _parse_game_config():
+    """
+    Parse the game config specified from the command line
+    """
+    config = get_game_config()
+
+    try:
+        game_defined_config = importlib.import_module(
+            "games.{}.config".format(config.game_name))
+        game_defined_params = game_defined_config.GAME_PARAMS
+    except ModuleNotFoundError:
+        raise GameConfigError("Game '{}' dosen\'t provide 'config.py'"
+            .format(config.game_name))
+    except AttributeError:
+        # The game doesn't define any game parameters
+        game_defined_params = {}
+
+    if not game_defined_params and len(config.game_params) != 0:
+        raise GameConfigError("Game '{}' takes no game parameter"
+            .format(config.game_name))
+    else:
+        _preprocess_game_param_dict(game_defined_params)
+        param_parser = get_parser_from_dict(game_defined_params)
+
+        # Replace the `game_params` with the parsed one
+        config.game_params = param_parser.parse_args(config.game_params)
+
+    return config
+
+def _preprocess_game_param_dict(param_dict):
+    """
+    Preprocess the game defined `GAME_PARAMS`
+    """
+    # Append the command of MLGame to the `game_usage`
+    # and set it to the `usage`
+    if (param_dict.get("()") and
+        param_dict["()"].get("game_usage")):
+        game_usage = str(param_dict["()"].pop("game_usage"))
+        param_dict["()"]["usage"] = (
+            "python MLGame.py [options] " + game_usage + "\n" +
+            "".ljust(24) + "[-i SCRIPTS/--input-module MODULES]")
+
+def _game_execution(game_config: GameConfig):
+    """
+    Execute the game
+
+    @param config The parsed game config
+    """
+    try:
+        game_defined_config = importlib.import_module(
+            "games.{}.config".format(game_config.game_name))
+        game_defined_processes = game_defined_config.PROCESSES
+
+        process_config = {
+            GameMode.MANUAL: game_defined_processes["manual_mode"],
+            GameMode.ML: game_defined_processes["ml_mode"]
+        }.get(game_config.game_mode)
+    except AttributeError:
+        raise GameConfigError("'PROCESSES' is not defined in '{}'"
+            .format(game_defined_config.__name__))
+    except KeyError as e:
+        raise GameConfigError("Cannot find {} in 'PROCESSES' in {}"
+            .format(game_defined_config.__name__, e))
+    # The exist of 'config.py' had been checked at '_parse_game_config',
+    # so no need to catch ModuleNotFoundError.
+
+    try:
+        _preprocess_process_config(process_config, game_defined_config)
+    except Exception as e:
+        raise GameConfigError("Error occurred while preprocessing 'PROCESSES' in '{}': "
+            "{}".format(game_defined_config.__name__, e))
+
+    if game_config.game_mode == GameMode.MANUAL:
+        _run_manual_mode(game_config, process_config)
+    else:
+        _run_ml_mode(game_config, process_config)
+
+def _preprocess_process_config(process_config, game_defined_config):
+    """
+    Replace the some string value in `process_config` to usable objects
+    """
+    # Replace the function name to the function defined in the game_defined_config
+    target_function = process_config["game"]["target"]
+    if isinstance(target_function, str):
+        process_config["game"]["target"] = game_defined_config.__dict__[target_function]
+
+def _run_manual_mode(game_config: GameConfig, process_config):
+    """
+    Execute the game specified in manual mode
+    """
+    game_process_config = process_config["game"]
+
+    # Append "game_config" at the beginning of function arguments
+    args = (game_config, ) + game_process_config.get("args", ())
+    kwargs = game_process_config.get("kwargs", {})
+
+    game_process_config["target"](*args, **kwargs)
+
+def _run_ml_mode(game_config: GameConfig, process_config):
+    """
+    Execute the game specified in ml mode
+    """
+    from .process import ProcessManager
+
+    process_manager = ProcessManager()
+
+    # Set game process #
+
+    game_process_config = process_config["game"]
+    args = (game_config, ) + game_process_config.get("args", ())
+    kwargs = game_process_config.get("kwargs", {})
+
+    process_manager.set_game_process(game_process_config["target"], args, kwargs)
+
+    # Set ml processes #
+
+    ml_process_names = [key for key in process_config.keys() if key != "game"]
+    for i in range(len(ml_process_names)):
+        process_name = ml_process_names[i]
+        args = process_config[process_name].get("args", ())
+        kwargs = process_config[process_name].get("kwargs", {})
+
+        # Assign the input modules to the ml processes
+        # If the number of provided modules is less than the number of processes,
+        # the last module is assigned to the rest processes.
+        module_id = (i if i < len(game_config.input_modules)
+            else len(game_config.input_modules) - 1)
+
+        process_manager.add_ml_process(game_config.input_modules[module_id],
+            process_name, args, kwargs)
+
+    process_manager.start()
