6de167301fee3f681e9d2210e043c36d260b9d6b
LanKuDot <airlanser@gmail.com>
2020-04-22 15:37:29 +0800

    Add group for flags in the help message

--- mlgame/gameconfig.py
+++ mlgame/gameconfig.py
@@ -23,35 +23,36 @@ def get_command_parser():
     parser.add_argument("game", type = str, nargs = "?",
         help = "the name of the game to be started")
     parser.add_argument("game_params", nargs = "*", default = None,
-        help = "the additional settings for the game")
+        help = "[optional] the additional settings for the game")
 
-    parser.add_argument("--version", action = "version", version = version)
-    parser.add_argument("-h", "--help", action = "store_true",
+    group = parser.add_argument_group(title = "functional options")
+    group.add_argument("--version", action = "version", version = version)
+    group.add_argument("-h", "--help", action = "store_true",
         help = "show this help message and exit. "
         "If the <game> is specified, show the help message of the game instead.")
-    parser.add_argument("-l", "--list", action = "store_true", dest = "list_games",
+    group.add_argument("-l", "--list", action = "store_true", dest = "list_games",
         help = "list available games. If the game in the 'games' directory "
         "provides 'config.py', it will be listed.")
-    parser.add_argument("-f", "--fps", type = int, default = 30,
+
+    group = parser.add_argument_group(title = "game execution options")
+    group.add_argument("-f", "--fps", type = int, default = 30,
         help = "the updating frequency of the game process [default: %(default)s]")
-    parser.add_argument("-m", "--manual-mode", action = "store_true", default = False,
+    group.add_argument("-m", "--manual-mode", action = "store_true",
         help = "start the game in the manual mode instead of "
         "the machine learning mode [default: %(default)s]")
-    parser.add_argument("-r", "--record", action = "store_true",
-        dest = "record_progress", default = False,
+    group.add_argument("-r", "--record", action = "store_true", dest = "record_progress",
         help = "pickle the game progress (a list of SceneInfo) to the log file. "
         "One file for a round, and stored in '<game>/log/' directory. "
         "[default: %(default)s]")
-    parser.add_argument("-1", "--one-shot", action = "store_true",
-        dest = "one_shot_mode", default = False,
+    group.add_argument("-1", "--one-shot", action = "store_true", dest = "one_shot_mode",
         help = "quit the game when the game is passed or is over. "
         "Otherwise, the game will restart automatically. [default: %(default)s]")
-    parser.add_argument("-i", "--input-script", type = str, nargs = '+',
+    group.add_argument("-i", "--input-script", type = str, nargs = '+',
         default = None, metavar = "SCRIPT",
         help = "specify user script(s) for the machine learning mode. "
         "The script must have function `ml_loop()` and "
         "be put in the '<game>/ml/' directory. [default: %(default)s]")
-    parser.add_argument("--input-module", type = str, nargs = '+',
+    group.add_argument("--input-module", type = str, nargs = '+',
         default = None, metavar = "MODULE",
         help = "specify the absolute import path of user module(s) "
         "for the machine learning mode. The module must have function "
